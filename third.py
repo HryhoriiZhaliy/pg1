91b78e15d9864f45992fc5c9379fd03f5c94ed6b
HryhoriiZhaliy <zhalih00@jcu.cz>
2024-10-20 19:38:48 +0000

    hot

--- third.py
+++ third.py
@@ -17,15 +17,30 @@ def je_prvocislo(cislo):
     36 * 1
     Jak vidite v druhe polovine se dvojice opakuji, tzn. v tomto pripade staci overit delitelnost pouze do 6 (vcetne)
     """
-    return False
+    if cislo <= 1:
+        return False
+    for i in range(2, int(cislo**0.5) + 1):
+        if cislo % i == 0:
+            return False
+    return True
 
 def vrat_prvocisla(maximum):
     """
     Funkce spocita vsechna prvocisla v rozsahu 1 az maximum a vrati je jako seznam.
     """
-    return [2,3,5]
+    prvocisla = []
+    for cislo in range(2, maximum + 1):
+        if je_prvocislo(cislo):
+            prvocisla.append(cislo)
+    return prvocisla
 
 if __name__ == "__main__":
-    cislo = input("Zadej maximum: ")
-    prvocisla = vrat_prvocisla(cislo)
-    print(prvocisla)
+    try:
+        cislo = int(input("Zadej maximum: "))
+        prvocisla = vrat_prvocisla(cislo)
+        print(prvocisla)
+    except ValueError:
+        print("Zadaná hodnota není platné číslo.")
+    #cislo = input("Zadej maximum: ")
+    #prvocisla = vrat_prvocisla(cislo)
+    #print(prvocisla)
