0f6f91954df06857f5d031f226ab7ac89b65adae
HryhoriiZhaliy <zhalih00@jcu.cz>
2024-11-14 09:41:12 +0000

    c

--- cviceni6.py
+++ cviceni6.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+from lxml import html
 
 def stahniurlavratnadpisy(url):
     nadpisy = []
@@ -11,7 +12,14 @@ def stahniurlavratnadpisy(url):
     if response.status_code != 200:
         print(f'nastala chyba, http code: {response.status_code}')
         return []
-    response.content
+    root = html.fromstring(response.content)
+    for h in ("h1", "h2", "h3", "h4", "h5"):
+        for h1 in root.xpath("//h1"):
+            h1_text = h1.text_content().strip()
+            if h1_text:
+                nadpisy.appent(h1_text)
+
+    
 
     return nadpisy
 
