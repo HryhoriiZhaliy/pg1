a93ac3995ba1d631e3410ea09de8407390e09437
HryhoriiZhaliy <zhalih00@jcu.cz>
2024-11-30 10:41:14 +0000

    a

--- cviceni8.py
+++ cviceni8.py
@@ -1,11 +1,36 @@
 def obvod_ctverce(delka_strany):
     # funkce vypocita obvod ctverce z delky jeho strany
-    pass
+    return 4 * delka_strany
 
 def obsah_ctverce(delka_strany):
     # funkce vypocita obsah ctverce z delky jeho strany
-    pass
+    return delka_strany ** 2
 
+def pocet_pismen(text, pismeno):
+    pocet = 0
+    for p in text:
+        if p == pismeno:
+            pocet +=1
+        
+    return pocet
+
+
+def index_pismene(text, pismeno):
+    indexy = []
+    i = 0
+    while i < len(text):
+        if text[i] == pismeno:
+            indexy.append(i)
+        i += 1
+    return indexy
+
+def fibonachi(maximum):
+    fib = [1, 1]
+    soucet = fib[-2] = fib[-1]
+    while soucet <= maximum:
+        fib.append(soucet)
+        soucet = fib[-2] = fib[-1]
+    return fib
 
 if __name__ == "__main__":
     print(obsah_ctverce(5))
--- cvicenitest.py
+++
@@ -1,6 +0,0 @@
-from cviceni8 import obsah_ctverce, obvod_ctverce
-
-
-def test_obsah_cvtverce():
-    assert obsah_ctverce(4) == 16
-    assert obsah_ctverce(5) == 25
